baff0b840d15c9097a129a80eea0749544f55bcf
AirtonSouza10 <airtonsouza.ti@gmail.com>
2023-05-31 12:12:22 -0300

    Projeto criado

---
+++ src/main/java/com/airtonsouza/crudsts/CrudStsApplication.java
@@ -0,0 +1,13 @@
+package com.airtonsouza.crudsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStsApplication.class, args);
+	}
+
+}
